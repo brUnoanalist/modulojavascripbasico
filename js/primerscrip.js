06e21720b4b25ec097aaa13ac553c544a507aef9
bruno fuenzalida <brunofuenzalida08@gmail.com>
2022-04-26 13:26:18 -0400

    primerscrip

---
+++ js/primerscrip.js
@@ -0,0 +1,23 @@
+var edad = 30;
+const edadmax = 70;
+var snack = "Mantecol";
+const snack_dia = 2;
+const precio_snack = 200;
+
+
+var snack_anio = 365 * 2;
+var snak_restantes = 40 * snack_anio;
+var dinero_total = precio_snack * snak_restantes
+
+const anio_actual = 2022
+
+
+var respuesta = prompt("ingrese su edad");
+var dias_anios = respuesta * 365
+
+
+alert("su edad actual es: " + respuesta, )
+alert("los dias de su edad es " + dias_anios)
+alert("Necesitaras  " + snak_restantes + " snack al llegar a los  " + edadmax + "  anios");
+alert("El dinero gastado sera: " + dinero_total);
+alert("Tu snack favorito es: " + snack)
